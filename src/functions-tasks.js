8c7e1c8a2c17417fbb1c8e6e4a2ca02fb5020504
Olga <kartoshkaslukom@gmail.com>
2024-04-06 17:47:22 +0200

    feat: add solutions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -180,8 +180,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function a(...args) {
+    const ars = args.map((x) => JSON.stringify(x)).join(',');
+    logFunc(`${func.name}(${ars}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${ars}) ends`);
+    return result;
+  };
 }
 
 /**
